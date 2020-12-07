9d2db8b45928b3b88708e8e37c31ee9ae4e48551
structsize <52183019+structsize@users.noreply.github.com>
2020-12-07 15:59:14 +0900

    [UPDATE] update internet status

--- app/src/main/java/com/example/client/start/SplashActivity.kt
+++ app/src/main/java/com/example/client/start/SplashActivity.kt
@@ -5,9 +5,12 @@ import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.ConnectivityManager.NetworkCallback
 import android.net.Network
+import android.net.NetworkCapabilities
 import android.net.NetworkRequest
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.client.MainActivity
@@ -20,6 +23,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
+            if(isNetworkAvailable(this)) {
                 if (getID() == 0) {
                     startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
                     finish()
@@ -27,11 +31,49 @@ class SplashActivity : AppCompatActivity() {
                     startActivity(Intent(this@SplashActivity, MainActivity::class.java))
                     finish()
                 }
+            } else {
+                finish()
+            }
         }, 3000)
     }
     private fun getID() : Int{
         val sp = getSharedPreferences("user", AppCompatActivity.MODE_PRIVATE)
         return sp.getInt("id", 0)
+    }
+
+    private fun isNetworkAvailable(context: Context?): Boolean {
+        if (context == null) return false
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                        return true
+                    }
+                }
+            }
+        } else {
+            try {
+                val activeNetworkInfo = connectivityManager.activeNetworkInfo
+                if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                    Log.i("update_statut", "Network is available : true")
+                    return true
+                }
+            } catch (e: Exception) {
+                Log.i("update_statut", "" + e.message)
+            }
+        }
+        Log.i("update_statut", "Network is available : FALSE ")
+        return false
     }
 
 }
